32decf03de0803ef00ca4cb3a095664dd8540774
KristinaGru <grudyte.kristina@gmail.com>
2023-02-27 09:27:34 +0100

    add a future age calculator

---
+++ javascript/javascript1/week1/index.js
@@ -0,0 +1,12 @@
+// Javascript warm up part one
+
+const myProfile = "https://www.freecodecamp.org/kristinagru";
+console.log("Here is a link to my freecodecamp profile: " + myProfile);
+
+// Age-ify (A future age calculator)
+
+const yearOfBirth = 1996;
+const yearFuture = 2027;
+const age = yearFuture - yearOfBirth;
+
+console.log("You will be " + age + " years old in " + yearFuture);
