6a2a1960f2b60bbcf3eea67162334d3cd56bd75e
KristinaGru <grudyte.kristina@gmail.com>
2023-03-07 21:06:38 +0100

    remove the event weekday function

--- javascript/javascript1/week2/index.js
+++ javascript/javascript1/week2/index.js
@@ -1,36 +1,4 @@
 // Event application
-
-const dayOfWeek = new Date().getDay();
-
-function getEventWeekday(day) {
-  const weekday = (dayOfWeek + day) % 7;
-  switch (weekday) {
-    case 0:
-      return 'Sunday';
-      break;
-    case 1:
-      return 'Monday';
-      break;
-    case 2:
-      return 'Tuesday';
-      break;
-    case 3:
-      return 'Wednesday';
-      break;
-    case 4:
-      return 'Thursday';
-      break;
-    case 5:
-      return 'Friday';
-      break;
-    case 6:
-      return 'Saturday';
-      break;
-  }
-}
-
-console.log(getEventWeekday(7));
-
 // Weather wear
 
 function getClothesSuggestion(temp) {
