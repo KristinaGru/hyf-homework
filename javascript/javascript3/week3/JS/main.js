49486f0f36e426b99277f672f97c372707f2f0e8
KristinaGru <grudyte.kristina@gmail.com>
2023-05-09 18:45:11 +0200

    add login/signup

--- javascript/javascript3/week3/JS/main.js
+++ javascript/javascript3/week3/JS/main.js
@@ -1,6 +1,8 @@
-const input = document.querySelector('input');
-const btn = document.querySelector('button');
+const input = document.getElementById('get-input');
+const btn = document.getElementById('get-btn');
 const display = document.getElementById('display');
+const userName = document.getElementById('login-name');
+let logedIn = false;
 
 async function getScreenshot(url) {
   const encodedurl = encodeURIComponent(url);
@@ -21,18 +23,21 @@ async function getScreenshot(url) {
   }
 }
 
-function saveScreenshot(url) {
+function createBtn(text) {
   const btn = document.createElement('button');
   btn.type = 'button';
-  btn.innerText = 'Save Screenshot';
+  btn.innerText = text;
   display.appendChild(btn);
+  return btn;
+}
+
+function saveScreenshot(url) {
+  const btn = createBtn('Save Screenshot');
   btn.addEventListener('click', async () => {
-    const screenshot = {
-      'url': url
-    };
+    const screenshot = { 'url': url };
     try {
-      const res = await fetch(
-        `https://crudcrud.com/api/${crudKey}/screenshots`,
+      await fetch(
+        `https://crudcrud.com/api/${crudKey}/screenshots-${userName.value}`,
         {
           headers: { 'Content-Type': 'application/json; charset=utf-8' },
           method: 'POST',
@@ -46,15 +51,12 @@ function saveScreenshot(url) {
 }
 
 function getScreenshots() {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = 'Show saved screenshots';
-  display.appendChild(btn);
+  const btn = createBtn('Show saved screenshots');
   const dataEl = document.createElement('div');
   btn.addEventListener('click', async () => {
     try {
       const res = await fetch(
-        `https://crudcrud.com/api/${crudKey}/screenshots`
+        `https://crudcrud.com/api/${crudKey}/screenshots-${userName.value}`
       );
       const data = await res.json();
       dataEl.innerText = '';
@@ -81,8 +83,8 @@ function deleteScreenshot(el, id) {
   el.appendChild(btn);
   btn.addEventListener('click', async () => {
     try {
-      const res = await fetch(
-        `https://crudcrud.com/api/${crudKey}/screenshots/${id}`,
+      await fetch(
+        `https://crudcrud.com/api/${crudKey}/screenshots-${userName.value}/${id}`,
         {
           method: 'DELETE'
         }
@@ -110,6 +112,68 @@ function displayScreenshot(url) {
 btn.addEventListener('click', async () => {
   const data = (await getScreenshot(input.value)).screenshotUrl;
   displayScreenshot(data);
-  saveScreenshot(data);
-  getScreenshots();
+  if (logedIn) {
+    saveScreenshot(data);
+    getScreenshots();
+  }
+});
+
+function showForm(formId, closeId) {
+  const form = document.getElementById(formId);
+  form.hidden = false;
+  document
+    .getElementById(closeId)
+    .addEventListener('click', () => (form.hidden = true));
+}
+
+document.getElementById('show-signup').addEventListener('click', () => {
+  showForm('signup-form', 'close-signup');
+});
+
+document.getElementById('show-login').addEventListener('click', () => {
+  showForm('login-form', 'close-login');
+});
+
+document.getElementById('signup').addEventListener('click', async () => {
+  const userName = document.getElementById('signup-name').value;
+  const userPwd = document.getElementById('signup-pwd').value;
+  if (userName && userPwd) {
+    const user = {
+      'name': userName,
+      'pwd': userPwd
+    };
+    try {
+      const res = await fetch(`https://crudcrud.com/api/${crudKey}/users`, {
+        headers: { 'Content-Type': 'application/json; charset=utf-8' },
+        method: 'POST',
+        body: JSON.stringify(user)
+      });
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    alert('Fill in username and password fields');
+  }
+});
+
+document.getElementById('login').addEventListener('click', async () => {
+  const userPwd = document.getElementById('login-pwd').value;
+  if (userName && userPwd) {
+    try {
+      const res = await fetch(`https://crudcrud.com/api/${crudKey}/users`);
+      const data = await res.json();
+      console.log(data);
+      data.forEach((element) => {
+        if (element.name === userName.value && element.pwd === userPwd) {
+          logedIn = true;
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    alert('Fill in username and password fields');
+  }
 });
