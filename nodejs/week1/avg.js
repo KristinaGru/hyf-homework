7b44e661b5f2bf9f90acd3a8fe53824e7db63850
KristinaGru <grudyte.kristina@gmail.com>
2023-06-08 11:44:14 +0200

    implement feedback

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -2,9 +2,9 @@ let total = 0;
 for (let i = 2; i < process.argv.length; i++) {
   total += +process.argv[i];
 }
-const avg = total / (process.argv.length - 2);
-if (isNaN(avg)) {
+if (isNaN(total) || process.argv.length < 3) {
   console.log('enter numbers');
 } else {
+  const avg = total / (process.argv.length - 2);
   console.log(avg);
 }
